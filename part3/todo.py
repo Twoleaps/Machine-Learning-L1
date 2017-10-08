58c43cc0defb9627087892f60e1d81c572c1bc90
mohitsh <mohitsh114@gmail.com>
2017-10-08 16:14:40 +0530

    feedback

--- part3/todo.py
+++ part3/todo.py
@@ -1,68 +1,61 @@
-class todo():
-    str1="python"
-    str2="programming"
-    def type(self):
-        type_name=type(todo.str1)
-        print(type_name)
+class Data():
+    def __init__(self, data): # initialize the class with some data (be it int, string, list)
+        self.data = data # keep a generic name to attributes it can be string, int, list, any data type
+        self.type = self.get_type()
 
-    def concat_data(self,a,b):
-        s=""
+    def get_type(self):
+        """"
+        assign type attribute
+        """
+        if isinstance(self.data, str): return 'string' 
+        if isinstance(self.data, int): return 'int'
+        if isinstance(self.data, list): return 'list'
+
+    def concat_data(self,a):
         try:
-            return s+a+b
+            return self.data + a
         except:
             return None
-    def reverse_data(self,a):
-        x=isinstance(a,int)
-        if x== True:
-            return None
-        else:
-            return a[::-1]
-            
-            
-    def return_last(self,a):
-        x=isinstance(a,int)
-        if x == True:
-            return None
-        else:
-            return a[-1]
 
-    def comparator(self,a,b):
-         x=isinstance(a,int)
+    def reverse_data(self):
+        x=isinstance(self.data,int)
+        if x is True: # proper way to check for True and False in Python
+            return None
+        else:
+            return self.data[::-1]
+            
+    def return_last(self):
+        x=isinstance(a,int)
+        if x is True:
+            return None
+        else:
+            return self.data[-1]
+
+    def comparator(self,a):
+         x=isinstance(self.data,int)
          y=isinstance(a,int)
-         if(x == True or y == True):
+         if(x is True or y is True):
             return None
          else:
-            return a==b
-    def mixture(self,a,b):
-        x=isinstance(a,int)
+            return self.data==a
+
+    def mixture(self,a):
+        x=isinstance(self.data,int)
         y=isinstance(a,int)
-        if(x == True or y == True):
+        if(x is True or y is True):
             return None
         else:
-            p=''.join(b)
-            return a+p
+            p=''.join([self.data, a])
+            return p
             
         
-    
-        
-        
-
-x=todo()
-x.type()
-a=x.concat_data("hello"," this works!")
-print(a)
-b=x.concat_data(1,2)
-print(b)
-y=[1,2,3,4,5]
-c=x.return_last(y)
-print(c)
-c=x.return_last("abc")
-print(c)
-c=x.return_last(1)
-print(c)
-print(x.comparator(y,y))
-print(x.comparator("abc","xyz"))
-print(x.comparator(1,2))
-print(x.mixture("aa",['b','b']))
-print(x.reverse_data("abcd"))
-print(x.reverse_data(y))
+if __name__ == "__main__":
+    x=Data("Python")
+    a=x.concat_data(" 2.7")
+    print(a)
+    b=x.concat_data(1)
+    print(b)
+    c=x.return_last()
+    print(c)
+    print(x.comparator("Python"))
+    print(x.comparator("abc"))
